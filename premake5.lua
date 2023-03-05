d1822072ea5f4fe54c87a3f9e850423c3eb33be6
Mikka <mikka@sendke.eu>
2023-03-05 17:39:53 +0100

    edit

--- premake5.lua
+++ premake5.lua
@@ -1,55 +1,57 @@
 workspace "gta-internal"
+    architecture ("x64")
     configurations { "Debug", "Release", "Dist" }
     startproject "Rizeq"
 
-    project "Rizeq"
-        kind "ConsoleApp"
-        architecture ("x64")
-        location "src/Rizeq"
-        language "C++"
-        cppdialect "C++20"
+project "Rizeq"
+    location "src/Rizeq"
+    files { "%{prj.location}/**.h", "%{prj.location}/**.cpp" }
+
+    kind "ConsoleApp"
+
+    language "C++"
+    cppdialect "C++20"
+    
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir ("bin/%{cfg.buildcfg}/intermediates")
+    
+    characterset ("MBCS")
+    
+    filter "configurations:Debug"
+        defines { "RZ_DEBUG" }
+        symbols "On"
+
+    filter "configurations:Release"
+        defines { "RZ_RELEASE" }
+        optimize "On"
+    
+    filter "configurations:Dist"
+        optimize "On"
+
+project "Injected"
+    location "src/Injected"
+    files { "%{prj.location}/**.h", "%{prj.location}/**.cpp" }
+
+    kind "SharedLib"
+
+    language "C++"
+    cppdialect "C++20"
+
+    pchheader "pch.h"
+    pchsource "%{prj.location}/pch.cpp"
+
+    targetdir "bin/%{cfg.buildcfg}"
+    objdir ("bin/%{cfg.buildcfg}/intermediates")
+    
+    characterset ("MBCS")
+
+    filter "configurations:Debug"
+        defines { "IJ_DEBUG" }
+        symbols "On"
         
-        characterset ("MBCS")
+    filter "configurations:Release"
+        defines { "IJ_RELEASE" }
+        optimize "On"
     
-        targetdir "bin/%{cfg.buildcfg}"
-        objdir ("bin/%{cfg.buildcfg}/intermediates")
-    
-        files { "%{prj.location}/**.h", "%{prj.location}/**.cpp" }
-    
-        filter "configurations:Debug"
-            defines { "DEBUG" }
-            symbols "On"
-    
-        filter "configurations:Release"
-            optimize "On"
-        
-        filter "configurations:Dist"
-            optimize "On"
-    
-    project "Injected"
-        kind "SharedLib"
-        architecture ("x64")
-        location "src/Injected"
-        language "C++"
-        cppdialect "C++20"
-    
-        pchheader "pch.h"
-        pchsource "%{prj.location}/pch.cpp"
-    
-        characterset ("MBCS")
-    
-        targetdir "bin/%{cfg.buildcfg}"
-        objdir ("bin/%{cfg.buildcfg}/intermediates")
-    
-        files { "%{prj.location}/**.h", "%{prj.location}/**.cpp" }
-    
-        filter "configurations:Debug"
-            defines { "DEBUG" }
-            symbols "On"
-    
-        filter "configurations:Release"
-            optimize "On"
-        
-        filter "configurations:Dist"
-            optimize "On"
-    
+    filter "configurations:Dist"
+        optimize "On"
