85cf192aa713df7d72d05637961b5e7b9c0e456b
Mikka <mikka@sendke.eu>
2023-03-05 17:19:34 +0100

    best changes

--- premake5.lua
+++ premake5.lua
@@ -1,53 +1,55 @@
 workspace "gta-internal"
     configurations { "Debug", "Release", "Dist" }
     startproject "Rizeq"
-    architecture ("x64")
 
-project "Rizeq"
-    kind "ConsoleApp"
-    location "src/Rizeq"
-    language "C++"
-    cppdialect "C++20"
+    project "Rizeq"
+        kind "ConsoleApp"
+        architecture ("x64")
+        location "src/Rizeq"
+        language "C++"
+        cppdialect "C++20"
+        
+        characterset ("MBCS")
     
-    characterset ("MBCS")
-
-    targetdir "bin/%{cfg.buildcfg}"
-    objdir ("bin/%{cfg.buildcfg}/intermediates")
-
-    files { "src/**.h", "src/**.cpp" }
-
-    filter "configurations:Debug"
-        defines { "DEBUG" }
-        symbols "On"
-
-    filter "configurations:Release"
-        optimize "On"
+        targetdir "bin/%{cfg.buildcfg}"
+        objdir ("bin/%{cfg.buildcfg}/intermediates")
     
-    filter "configurations:Dist"
-        optimize "On"
-
-project "Injected"
-    kind "SharedLib"
-    location "src/Injected"
-    language "C++"
-    cppdialect "C++20"
-
-    pchheader "pch.h"
-    pchsource "pch.cpp"
-
-    characterset ("MBCS")
-
-    targetdir "bin/%{cfg.buildcfg}"
-    objdir ("bin/%{cfg.buildcfg}/intermediates")
-
-    files { "src/**.h", "src/**.cpp" }
-
-    filter "configurations:Debug"
-        defines { "DEBUG" }
-        symbols "On"
-
-    filter "configurations:Release"
-        optimize "On"
+        files { "%{prj.location}/**.h", "%{prj.location}/**.cpp" }
     
-    filter "configurations:Dist"
-        optimize "On"
+        filter "configurations:Debug"
+            defines { "DEBUG" }
+            symbols "On"
+    
+        filter "configurations:Release"
+            optimize "On"
+        
+        filter "configurations:Dist"
+            optimize "On"
+    
+    project "Injected"
+        kind "SharedLib"
+        architecture ("x64")
+        location "src/Injected"
+        language "C++"
+        cppdialect "C++20"
+    
+        pchheader "pch.h"
+        pchsource "%{prj.location}/pch.cpp"
+    
+        characterset ("MBCS")
+    
+        targetdir "bin/%{cfg.buildcfg}"
+        objdir ("bin/%{cfg.buildcfg}/intermediates")
+    
+        files { "%{prj.location}/**.h", "%{prj.location}/**.cpp" }
+    
+        filter "configurations:Debug"
+            defines { "DEBUG" }
+            symbols "On"
+    
+        filter "configurations:Release"
+            optimize "On"
+        
+        filter "configurations:Dist"
+            optimize "On"
+    
--- src/Rizeq/Rizeq.cpp
+++ src/Rizeq/Rizeq.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <Windows.h>
 
 int main()
 {
 	std::cout << "Hello World!\n";
 }
-
